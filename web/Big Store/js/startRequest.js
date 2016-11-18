38b043898eac9c148d27465698bf75d55f89e4f8
Ricardo Ferreira da Silva <tansvanio@hotmail.com>
2016-11-18 17:45:52 +0000

    client

--- web/Big Store/js/startRequest.js
+++ web/Big Store/js/startRequest.js
@@ -18,6 +18,7 @@ function processArticles(response)
 	var articles = JSON.parse(response);
 	//alert(articles[4].CodArtigo);
 	var articleHolder = document.getElementById("article-holder");
+	var descriptionHolder = document.getElementById("parentID");
 	for(i=0;i<8;i++)
 	{
 		var codArtigo = articles[i].CodArtigo
@@ -25,7 +26,7 @@ function processArticles(response)
 		articleHolder.innerHTML += `<div class='col-md-3 pro-1'>
 								<div class='col-m'>
 									<a href='#' data-toggle='modal' data-target='#myModal24' class='offer-img'>
-										<img src='' class='img-responsive' alt=''>
+										<img src='images/of2.png' class='img-responsive' alt=''>
 									</a>
 									<div class='mid-1'>
 										<div class='women'>
@@ -44,4 +45,35 @@ function processArticles(response)
 								</div>
 							</div>`
 	}
+	document.body.insertAdjacentHTML('beforeend',`<div class="modal fade" id="myModal24" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content modal-info">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
+						</div>
+						<div class="modal-body modal-spa">
+								<div class="col-md-5 span-2">
+											<div class="item">
+												<img src="images/of24.png" class="img-responsive" alt="">
+											</div>
+								</div>
+								<div class="col-md-7 span-1 ">
+									<h3>Wheat(500 g)</h3>
+									<p class="in-para"> There are many variations of passages of Lorem Ipsum.</p>
+									<div class="price_single">
+									  <span class="reducedfrom "><del>$2.00</del>$1.50</span>
+									
+									 <div class="clearfix"></div>
+									</div>
+									<h4 class="quick">Quick Overview:</h4>
+									<p class="quick_desc"> Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; es</p>
+									 <div class="add-to">
+										   <button class="btn btn-danger my-cart-btn my-cart-btn1 " data-id="24" data-name="Wheat" data-summary="summary 24" data-price="1.50" data-quantity="1" data-image="images/of24.png">Add to Cart</button>
+										</div>
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+						</div>
+					</div>
+				</div>`);
 }
