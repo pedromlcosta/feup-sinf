f93695c41ab135de82aa3586e6f6b26350d8f09d
Ricardo Ferreira da Silva <tansvanio@hotmail.com>
2016-11-18 10:57:58 +0000

    request dev
    not working yet

--- web/Big Store/js/requests.js
+++ web/Big Store/js/requests.js
@@ -1,11 +1,13 @@
 function requestArticles()
 {
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-        alert(xhr.responseText);
-    }
+        var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+	    if (xhr.readyState == XMLHttpRequest.DONE) {
+	        alert(xhr.responseText);
+	    }
+	 	xhr.open('GET', 'http://localhost:49822/api/artigos', true);
+        xhr.setRequestHeader("Content-type", "application/json");
+	  	xhr.send();
+		}
 }
-  xhr.open('GET', 'http://example.com', true);
-  xhr.send(null);
-}
+window.onload = requestArticles;
