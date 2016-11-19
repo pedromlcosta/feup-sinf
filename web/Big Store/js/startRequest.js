4bafc5dff50f4e928091e3b548c69fa0992449f0
Ricardo Ferreira da Silva <tansvanio@hotmail.com>
2016-11-19 22:27:48 +0000

    Search Function and other improvements

--- web/Big Store/js/startRequest.js
+++ web/Big Store/js/startRequest.js
@@ -1,3 +1,5 @@
+var articles;
+var current_filtered_articles = new Array();
 window.onload = getArticlesRequest;
 function getArticlesRequest()
 {
@@ -5,21 +7,25 @@ function getArticlesRequest()
 	xhttp.onreadystatechange = function() 
 	{
     if (this.readyState == 4 && this.status == 200) {
-       		processArticles(xhttp.responseText);
+    		articles = JSON.parse(xhttp.responseText);
+       		processArticles(articles,0,8);
     	}
 	};
 	xhttp.open("GET", "http://localhost:49822/api/artigos", true);
 	xhttp.setRequestHeader("Content-Type", "text/json");
 	xhttp.send();
 }
-function processArticles(response)
+function processArticles(articles,start_index,end_index)
 {
 	var i;
-	var articles = JSON.parse(response);
 	//alert(articles[4].CodArtigo);
 	var articleHolder = document.getElementById("article-holder");
 	var descriptionHolder = document.getElementById("myModal5");
-	for(i=0;i<8;i++)
+	//For the modals.
+	var j=0;
+	articleHolder.innerHTML = "";
+	if(articles.length < end_index) end_index = articles.length;
+	for(i=start_index;i<end_index;i++)
 	{
 		var codArtigo = articles[i].CodArtigo
 		var desc = articles[i].DescArtigo;
@@ -28,7 +34,7 @@ function processArticles(response)
 		var stock = articles[i].StockActual;
 		articleHolder.innerHTML += `<div class='col-md-3 pro-1'>
 						<div class='col-m'>
-							<a href='#' data-toggle='modal' data-target='#myModal`+i+`' class='offer-img'>
+							<a href='#' data-toggle='modal' data-target='#myModal`+j+`' class='offer-img'>
 								<img src='images/of2.png' class='img-responsive' alt=''>
 							</a>
 							<div class='mid-1'>
@@ -55,5 +61,20 @@ function processArticles(response)
 		$("#myModal"+i+" .btn.btn-danger.my-cart-btn.my-cart-btn1").attr("data-price",price);
 		$("#myModal"+i+" .btn.btn-danger.my-cart-btn.my-cart-btn1").attr("data-quantity","1");
 		$("#myModal"+i+" .btn.btn-danger.my-cart-btn.my-cart-btn1").attr("data-image","images/of28.png");
+		j++;
 	}
+}
+function filterArticles(string)
+{
+	current_filtered_articles = [];
+	for(var i=0;i<articles.length;i++)
+	{
+		var desc = articles[i].DescArtigo
+		if(desc.toLowerCase().indexOf(string) !== -1)
+		{
+			current_filtered_articles.push(articles[i]);
+		}
+	}
+	if(string != "" ) processArticles(current_filtered_articles,0,8);
+	else processArticles(articles,0,8);
 }
