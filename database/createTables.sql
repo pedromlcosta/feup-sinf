b8f6b6d488ed0e2ba85d08ffd0b95b8a60e0f441
pedromlcosta <up201303973@fe.up.pt>
2016-11-23 01:47:02 +0000

    Login is now a modal popup

--- database/createTables.sql
+++ database/createTables.sql
@@ -1,15 +1,17 @@
 ﻿SET SCHEMA 'sinf';
 
-DROP TABLE IF EXISTS Utilizador CASCADE; 
+DROP TABLE IF EXISTS User CASCADE; 
 DROP TABLE IF EXISTS Reviews CASCADE; 
 DROP TABLE IF EXISTS Product CASCADE;
 
-CREATE TABLE IF NOT EXISTS Utilizador(
+CREATE TABLE IF NOT EXISTS User(
 code SERIAL PRIMARY KEY,
-password VARCHAR(256) NOT NULL,
-email TEXT NOT NULL UNIQUE
+email TEXT NOT NULL UNIQUE,
+type VARCHAR(16) NOT NULL,
+password VARCHAR(256) NOT NULL
 );
 
+/*
 CREATE TABLE IF NOT EXISTS Product(
 code SERIAL PRIMARY KEY,
 primaveraCode INTEGER NOT NULL UNIQUE,
@@ -18,23 +20,24 @@ img TEXT NOT NULL -- link to img of a product
 
 CREATE TABLE IF NOT EXISTS Reviews(
 code SERIAL PRIMARY KEY,
-utilizador INTEGER REFERENCES Utilizador(code),
+utilizador INTEGER REFERENCES User(code),
 productCode INTEGER REFERENCES Product(code),  --subject to change and verify with primavera
 review TEXT NOT NULL,
 score SMALLINT NOT NULL,
 tsv tsvector
 );
+*/
 
 --http://stackoverflow.com/questions/9807909/are-email-addresses-case-sensitive to justify the trigger
 CREATE OR REPLACE FUNCTION lowerCaseEmailFunction() RETURNS trigger AS $$
 BEGIN
  NEW.email = lower(NEW.email);
  RETURN NEW;
-END 
+END  
 $$ LANGUAGE 'plpgsql'; 
 
 CREATE TRIGGER lowerCaseEmail
-BEFORE INSERT OR UPDATE ON Utilizador 
+BEFORE INSERT OR UPDATE ON User 
 FOR EACH ROW
 EXECUTE PROCEDURE lowerCaseEmailFunction();
 
@@ -52,11 +55,11 @@ END
 $$ LANGUAGE 'plpgsql'; 
 
 CREATE TRIGGER validateEmail
-BEFORE INSERT OR UPDATE ON Utilizador 
+BEFORE INSERT OR UPDATE ON User 
 FOR EACH ROW
 EXECUTE PROCEDURE validateEmailFunction();
 
-INSERT INTO Utilizador (password,email) VALUES ('Admin','hi@gmail.com');
-INSERT INTO Utilizador (password,email) VALUES ('Admin','TEST@gmail.com');
-INSERT INTO Utilizador (password,email) VALUES ('Admin','TESTsdasdasd.com');
+INSERT INTO User (email, type, password) VALUES ('hi@gmail.com', 'admin', '123');
+INSERT INTO User (email, type, password) VALUES ('TEST@gmail.com', 'client', '123');
+INSERT INTO User (email, type, password) VALUES ('TESTsdasdasd.com', 'client', '123');
 
