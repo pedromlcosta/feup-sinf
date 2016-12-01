849811ff96d394966521cc1fce2818d5d506c873
pedromlcosta <up201303973@fe.up.pt>
2016-12-01 23:33:46 +0000

    Postgres integration working

---
+++ app/FirstREST/Connection/ConnectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using Npgsql;
+
+namespace FirstREST {
+    public static class ConnectionFactory {
+
+        public static NpgsqlConnection MakePostgresConnection() {
+
+            NpgsqlConnection con = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["PostgresConnection"].ToString());
+            return con;
+
+        }
+    }
+}
