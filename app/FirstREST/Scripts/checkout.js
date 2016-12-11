e9122f1a903250ebb603d5771291e2fc0c28c57a
Ricardo Ferreira da Silva <tansvanio@hotmail.com>
2016-12-11 17:54:53 +0000

    getCart function in checkout.js

--- app/FirstREST/Scripts/checkout.js
+++ app/FirstREST/Scripts/checkout.js
@@ -1,5 +1,21 @@
-$("checkout_button").click(function(){
-    $.get("http://localhost:49822/api/artigos", function(data, status){
-        alert("Data: " + data + "\nStatus: " + status);
-    });
-});
+function getCart() {
+    var list = [];
+    var tr = $("#my-cart-table tbody").children();
+    for (var i = 0; i < tr.length - 2; i++)
+    {
+        var title = tr[i].getAttribute("title");
+        var id = tr[i].getAttribute("data-id");
+        console.log(id);
+        var td = tr[i].children;
+        var input = td[3].children;
+        var quantity = input[0].getAttribute("value");
+        console.log(quantity);
+        var data = {
+            'id': id,
+            'quantity': quantity 
+        };
+        list.push(data);
+    }
+    return list;
+}
+   
