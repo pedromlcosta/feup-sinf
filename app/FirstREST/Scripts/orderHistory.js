8068067f98bfe544a97832001e5ea6801a421da6
Francisco Pinho <up201303744@fe.up.pt>
2016-12-14 16:50:39 +0000

    oopsie

--- app/FirstREST/Scripts/orderHistory.js
+++ app/FirstREST/Scripts/orderHistory.js
@@ -42,6 +42,7 @@ function processOrders(orders,start_index,end_index)
         var totalDesc = orders[i].TotalDesc;
         var soldItems = orders[i].LinhasDoc;
         var final = totalMerc-totalDesc;
+        var pricewithoutIva=totalMerc-totalIva
         orderHolder.innerHTML += `<div class='col-md4 pro-1'>
 						<div class='col-cs'>
 							<a href='#' data-toggle='modal' data-target='#EncomendaModal' onclick="getOrderDetails('`+orderID+`');">
@@ -53,7 +54,7 @@ function processOrders(orders,start_index,end_index)
                                     <label>Order Date:</label><span>`+data+`</span><br>                      
                                     <br>
                                     <label>Cost Breakdown:</label>
-									<p>+ Preço:€`+totalMerc-totalIva+`</p>
+									<p>+ Preço:€`+pricewithoutIva+`</p>
                                     <p>+ IVA:€`+totalIva+`</p>
                                     <p>- Desconto:€`+totalDesc+`</p>
                                     <p>-----------------------------------------</p>
