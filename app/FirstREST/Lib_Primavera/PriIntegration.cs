2b9fe47fc25f431e68c76666fe75088f9f7572ff
unknown <joaofilipe26@gmail.com>
2016-12-15 00:39:07 +0000

    function that return img from db and function that adds/updates img from db

--- app/FirstREST/Lib_Primavera/PriIntegration.cs
+++ app/FirstREST/Lib_Primavera/PriIntegration.cs
@@ -8,6 +8,8 @@ using Interop.StdPlatBS900;
 using Interop.StdBE900;
 using Interop.GcpBE900;
 using ADODB;
+using Npgsql;
+using System.Data;
 
 namespace FirstREST.Lib_Primavera
 {
@@ -499,6 +501,130 @@ namespace FirstREST.Lib_Primavera
                 }
             }
             return returnFlag;
+        }
+
+        public static string getArtigoImg(string codArtigo)
+        {
+            string imgPath = "default.jpg";
+            string sql = "SELECT img FROM  Product WHERE primaveraCode = :code;";
+            NpgsqlConnection conn = null;
+            // Creates and opens connection to the postgres DB
+            conn = ConnectionFactory.MakePostgresConnection();
+            conn.Open();
+            // Rudimentar way to check if user exists and password coincides
+            NpgsqlCommand command;
+            try
+            {
+                command = new NpgsqlCommand();
+                command.Connection = conn;
+                command.CommandText = sql;
+                command.Parameters.Add(new NpgsqlParameter("code", DbType.String));
+                command.Parameters[0].Value = codArtigo;
+                command.Prepare();
+                NpgsqlDataReader dr = command.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+
+                        imgPath = (string)dr["img"];
+                        Debug.Write("\nImgPAath: " + imgPath);
+                    }
+                }
+                dr.Close();
+            }
+            catch (Exception msg)
+            {
+                Debug.Write(msg);
+                // something went wrong, and you wanna know why
+            }
+            finally
+            {
+                if (conn != null)
+                    conn.Close();
+
+            }
+            return imgPath;
+        }
+       
+        public static bool uploadProductImg(string codArtigo, string imgPath)
+        {
+            NpgsqlConnection conn = null;
+            try
+            {
+                // Creates and opens connection to the postgres DB
+                conn = ConnectionFactory.MakePostgresConnection();
+                conn.Open();
+                // Rudimentar way to check if user exists and password coincides
+                NpgsqlCommand command;
+                try
+                {
+                    string sql = "SELECT * FROM  Product WHERE primaveraCode = :code;";
+
+
+                    command = new NpgsqlCommand();
+                    command.Connection = conn;
+                    command.CommandText = sql;
+                    command.Parameters.Add(new NpgsqlParameter("code", DbType.String));
+                    command.Parameters[0].Value = codArtigo;
+                    command.Prepare();
+                    NpgsqlDataReader dr = command.ExecuteReader();
+
+
+                    if (dr.HasRows)
+                    {
+                        dr.Close();
+                        command = new NpgsqlCommand();
+                        command.Connection = conn;
+                        sql = "UPDATE Product SET img = :imgLink WHERE primaveraCode = :code;";
+                        command.CommandText = sql;
+                        command.Parameters.Add(new NpgsqlParameter("imgLink", DbType.String));
+                        command.Parameters.Add(new NpgsqlParameter("code", DbType.String));
+                        command.Parameters[0].Value = imgPath;
+                        command.Parameters[1].Value = codArtigo;
+                        command.Prepare();
+                        if (command.ExecuteNonQuery() == 1)
+                            return true;
+                    }
+                    else
+                    {
+                        //Insert
+                        dr.Close();
+                        sql = " INSERT INTO Product(primaveraCode,img) VALUES(:code,:img);";
+                        command = new NpgsqlCommand();
+                        command.Connection = conn;
+                        command.CommandText = sql;
+                        command.Parameters.Add(new NpgsqlParameter("code", DbType.String));
+                        command.Parameters.Add(new NpgsqlParameter("img", DbType.String));
+                        command.Parameters[0].Value = codArtigo;
+                        command.Parameters[1].Value = imgPath;
+                        command.Prepare();
+                        if (command.ExecuteNonQuery() == 1)
+                            return true;
+
+                    }
+                    return false;
+
+                }
+                catch (Exception ex)
+                {
+                    Debug.Write(ex);
+                    return false;
+                }
+
+            }
+            catch (Exception msg)
+            {
+                Debug.Write(msg);
+                // something went wrong, and you wanna know why
+            }
+            finally
+            {
+                if (conn != null)
+                    conn.Close();
+
+            }
+            return false;
         }
         #endregion Artigo
 
